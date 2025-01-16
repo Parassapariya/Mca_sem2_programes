a1b1c23d81ccd2b282cad553737fab3cea0d4dd6
Paras Sapariya <104016330+Parassapariya@users.noreply.github.com>
2025-01-16 15:12:32 +0530

    programe 2

--- pro_2.py
+++ pro_2.py
@@ -1,1 +1,20 @@
-#write a programe 
+#write a program to input 2 numbers and one arithmatic oprator. perfom oprations as per arithmatic oprator which is given as input
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+oprator = input("Enter arithmatic oprator (+, -, *, /): ")
+
+if oprator == "+":
+    print(num1 + num2)
+elif oprator == "-":
+    print(num1 - num2)
+elif oprator == "*":
+    print(num1 * num2)
+elif oprator == "/":
+    if num2 != 0:
+        print(num1 / num2)
+    else:
+        print("Error! Division by zero is not allowed.")
+else:
+    print("Error! Invalid arithmatic oprator.")
+
+
