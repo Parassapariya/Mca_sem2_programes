833286472137922ce29a784ad9d1cd27b595cbbc
Paras Sapariya <104016330+Parassapariya@users.noreply.github.com>
2025-01-20 14:24:03 +0530

    programe 6

--- python/pro_6.py
+++ python/pro_6.py
@@ -1,10 +1,19 @@
 #histograme print lines of * we can  input the numbers 
 
+'''
+
 print("Enter the numbers separated by space")
-
 numbers = input()
-
 numbers = [int(x) for x in numbers.split()]
-
 for i in range(len(numbers)):
     print("*" * numbers[i])
+
+'''
+
+def histogram(values):
+    for value in values:
+        print('*' * value)
+
+numbers = input()
+numbers = [int(x) for x in numbers.split()]
+histogram(numbers)
