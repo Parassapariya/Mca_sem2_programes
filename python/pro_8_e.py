62c28c3f3ff10cb5a391a004f90771d2cd6cb3b8
Paras Sapariya <104016330+Parassapariya@users.noreply.github.com>
2025-01-27 11:12:58 +0530

    programe 8 compleated

---
+++ python/pro_8_e.py
@@ -0,0 +1,14 @@
+#check whether string entered is a palindrome or not
+
+def reverse_string(s):
+    return s[::-1]
+
+def is_palindrome(string):
+    return string == reverse_string(string)
+
+string = input("Enter a string: ")
+if is_palindrome(string):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
+    
