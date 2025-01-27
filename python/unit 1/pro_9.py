ecb7b7e0e5f6eff8884cbfd3a5ca1317caf58e9d
Paras Sapariya <104016330+Parassapariya@users.noreply.github.com>
2025-01-27 11:16:51 +0530

    programe 9 completed

---
+++ python/unit 1/pro_9.py
@@ -0,0 +1,21 @@
+#Write a program to create a function which accepts 2 numbers and one arithmetic operator. Return the answer accordingly.
+
+def arithmetic_operations(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error! Division by zero is not allowed."
+    else:
+        return "Error! Invalid arithmetic operator."
+    
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+operator = input("Enter arithmetic operator (+, -, *, /): ")
+print(arithmetic_operations(num1, num2, operator))
