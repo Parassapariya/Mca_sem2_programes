8d8a133ce5bdb8bf7ca7c3dd34e90b22bfa38075
Paras Sapariya <104016330+Parassapariya@users.noreply.github.com>
2025-02-03 15:28:12 +0530

    chnages

---
+++ networking/client_server/client.py
@@ -0,0 +1,19 @@
+import socket
+HOST = 'localhost'
+PORT = 8080
+# Create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connect to the server
+client_socket.connect((HOST, PORT))
+# Print the connection information
+print("Connected to {}:{}".format(HOST, PORT))
+# Send and receive messages
+while True:
+    message = input("Enter your message: ")
+    client_socket.sendall(message.encode())
+    if message.lower() == 'bye':
+        break
+    data = client_socket.recv(1024).decode()
+    print("Server:", data)
+# Close the connection
+client_socket.close()
---
+++ networking/client_server/server.py
@@ -0,0 +1,1 @@
+#using socket programming clent server message passing  
