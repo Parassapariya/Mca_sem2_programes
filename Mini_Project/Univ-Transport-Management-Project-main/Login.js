dac58244f438a37d3333ace1c4b1dc4d45543a0b
Paras Sapariya <104016330+Parassapariya@users.noreply.github.com>
2025-02-17 13:54:53 +0530

    changes

---
+++ Mini_Project/Univ-Transport-Management-Project-main/Login.js
@@ -0,0 +1,140 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC2MOP43X8qny9j0YrbiORAjp_h1iuuRAQ",
+  authDomain: "eventmanagerlogin.firebaseapp.com",
+  projectId: "eventmanagerlogin",
+  storageBucket: "eventmanagerlogin.appspot.com",
+  messagingSenderId: "378697750470",
+  appId: "1:378697750470:web:de411865be3e7bd93704c7"
+};
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  // Initialize variables
+  const auth = firebase.auth()
+  const database = firebase.database()
+  
+  // Set up our register function
+  function register () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+
+  
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is improper!')
+      return
+      // Don't continue running the code
+    }
+
+   
+    // Move on with Auth
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        email : email,
+        password : password,
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).set(user_data)
+  
+      // DOne
+      alert('User Created!!')
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+  }
+  
+  // Set up our login function
+  function login () {
+    // Get all our input fields
+    email = document.getElementById('email').value
+    password = document.getElementById('password').value
+  
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+      alert('Email or Password is improper!')
+      return
+      // Don't continue running the code
+    }
+  
+    auth.signInWithEmailAndPassword(email, password)
+    .then(function() {
+      // Declare user variable
+      var user = auth.currentUser
+  
+      // Add this user to Firebase Database
+      var database_ref = database.ref()
+  
+      // Create User data
+      var user_data = {
+        last_login : Date.now()
+      }
+  
+      // Push to Firebase Database
+      database_ref.child('users/' + user.uid).update(user_data)
+  
+      // DOne
+      alert('User Logged in')
+      window.location="AddTransport.html";
+    // document.write("Please wait... directing to another page");
+    // setTimeout('myfunction()',2000);
+  
+    })
+    .catch(function(error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code
+      var error_message = error.message
+  
+      alert(error_message)
+    })
+    
+  }
+  
+  // Validate Functions
+  function validate_email(email) {
+    expression = /^[^@]+@\w+(\.\w+)+\w$/
+    if (expression.test(email) == true) {
+      // Email is good
+      return true
+    } else {
+      // Email is not good
+      return false
+    }
+  }
+  
+  function validate_password(password) {
+    // Firebase only accepts lengths greater than 6
+    if (password < 6) {
+      return false
+    } else {
+      return true
+    }
+  }
+  
+  function validate_field(field) {
+    if (field == null) {
+      return false
+    }
+  
+    if (field.length <= 0) {
+      return false
+    } else {
+      return true
+    }
+  }
---
+++ Mini_Project/Univ-Transport-Management-Project-main/Project overview.md
@@ -0,0 +1,15 @@
+# Univ-Transport-Management
+University Transport Management System is a website developed to help students of university find details of the transport facilities who are in collaboration with 
+university. 
+
+The system consists of two modules:
+Student module: Provides complete detailed information regarding transport facilities available from their cities to university,
+                Students can book or delete the booking and also can submit a feedback for the same.
+                
+Trasnport Authority Module: This module is for the transport authority who will add the details regarding the transport services offered by them. 
+                            They can add, update or delete their details as per the requirements.
+                            Can display the booking details of stundents.
+                            Can view feedbacks provided by students.
+                            
+The project is developed using HTML, CSS, Javascript and Firebase.
+
