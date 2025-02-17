4cd13251a433acec8bed6015f1f679a9bcf25c6b
Paras Sapariya <104016330+Parassapariya@users.noreply.github.com>
2025-02-17 13:55:06 +0530

    changes

--- Mini_Project/ApnaBharat-Bus-Booking-Reservation-System-master/bus-booking-system/__main__.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.chdir("./bus-booking-system")
-os.system("python main.py")
--- Mini_Project/busmanagement_project-main/students/forms.py
+++
@@ -1,8 +0,0 @@
-# students/forms.py
-from django import forms
-from .models import Student
-
-class StudentForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = ['first_name', 'last_name', 'email', 'phone_number', 'student_id', 'course']
